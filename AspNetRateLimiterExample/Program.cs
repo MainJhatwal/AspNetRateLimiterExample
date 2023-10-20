f168dd5fc2358f012284bba0894a322e52925e63
Main Pal <main.pal@bcstechnology.com.au>
2023-10-20 11:53:54 +0530

    Add project files.

---
+++ AspNetRateLimiterExample/Program.cs
@@ -0,0 +1,42 @@
+
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+
+// Configure rate limiting options
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+
+app.UseAuthorization();
+app.UseIpRateLimiting();
+app.MapControllers();
+
+app.Run();
---
+++ AspNetRateLimiterTestClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetRateLimiterTestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = GetClient();
+            int TotalRuns = 0;
+            while (TotalRuns < 1000)
+            {
+                try
+                {
+                    // Make an HTTP GET request
+                    HttpResponseMessage response = httpClient.GetAsync("WeatherForecast").Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine(content);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Request failed with status code: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+                TotalRuns++;
+                Task.Delay(100).Wait();
+                Console.WriteLine($"{TotalRuns} - WeatherForecast API calls completed");
+
+            }
+            Console.ReadKey();
+            // Get client
+            HttpClient GetClient()
+            {
+                var services = new ServiceCollection();
+                services.AddHttpClient("RateLimiterExampleApi", client =>
+                {
+                    client.BaseAddress = new Uri("https://localhost:5157");
+                });
+
+                // Build the service provider and get an instance of HttpClient
+                IServiceProvider serviceProvider = services.BuildServiceProvider();
+                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+                return httpClientFactory.CreateClient("RateLimiterExampleApi");
+
+            }
+
+        }
+
+    }
+
+}
